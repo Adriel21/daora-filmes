76866e9dc49447a7691a60e8aaecec8d667ee345
Adriel Lira <adrieldiasdossantoslira@hotmail.com>
2022-11-09 10:50:33 -0300

    Finalização de exercício e desafios

--- src/screens/FormBusca.js
+++ src/screens/FormBusca.js
@@ -1,10 +1,25 @@
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native'
+import { StyleSheet, Text, View, SafeAreaView, TextInput, Button, Alert } from 'react-native'
+import { Ionicons } from "@expo/vector-icons";
+import { useState } from 'react';
 
 const FormBusca = () => {
+  // const inputFilme = (event) => setFilme(event.target.value);
+
+  const [filme, setFilme] = useState("");
+  const titulo = filme == '' ? 'Ops!' : 'Você procurou por:';
+  const resultado = filme == '' ? 'Você deve digitar o nome de um filme' : filme;
+  console.log(filme);
+
   return (
     
-      <SafeAreaView>
-          <Text>FormBusca</Text>
+      <SafeAreaView style={estilos.container}>
+          <Text style={estilos.titulo}>Start Trek? O Poderoso Chefão? A trilogia Senhor dos Anéis?</Text>
+          <Text style={estilos.subtitulo}>Localize um fime que você viu ou gostaria de ver!</Text>
+          <View style={estilos.caixa}>
+            <Ionicons name="film" size={46} color="black" style={estilos.icon}/>
+            <TextInput style={estilos.input} placeholder="Filme..." onChangeText={setFilme}></TextInput>
+          </View>
+          <Button title='Procurar' style={estilos.botao} onPress={() => Alert.alert(titulo, resultado)}/>
       </SafeAreaView>
     
   )
@@ -12,4 +27,34 @@ const FormBusca = () => {
 
 export default FormBusca
 
-const styles = StyleSheet.create({})
+const estilos = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+  },
+  titulo: {
+    fontSize: 16,
+  },
+  subtitulo: {
+    fontSize: 16,
+    marginTop: 10,
+  },
+  caixa: {
+    marginTop: 10,
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+    marginBottom: 20,
+  },
+ 
+  input: {
+    height: 40,
+    borderWidth: 1,
+    padding: 10,
+    width: "80%",
+  },
+  botao: {
+    backgroundColor: "#5456a6",
+    height: 25,
+  },
+})
