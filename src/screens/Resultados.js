f096156d3a96f10ce21dac3b7df2072896c4d56d
Adriel Lira <adrieldiasdossantoslira@hotmail.com>
2022-11-11 10:45:54 -0300

    Carregamento de imagens da API

--- src/screens/Resultados.js
+++ src/screens/Resultados.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { StyleSheet, Text, View, SafeAreaView, Image } from 'react-native'
+import { StyleSheet, Text, View, SafeAreaView, Image, ScrollView } from 'react-native'
 import apiKey from '../../apiKey';
 import Loading from '../components/Loading';
 import api from '../services/api';
@@ -45,18 +45,29 @@ const Resultados = ( {route} ) => {
     }, [])
 
 
-    if(loading) return <Loading />
 
   return (
     <SafeAreaView style={estilos.container}>
       <Text>Você buscou por: {filme}</Text>
-      {resultados.map(({id, poster_path, original_title}) => (
-        <View>
-            <Text>{original_title}</Text>
-            <Image source={poster_path}/>
-        </View>
+
+    {/* Expressão if evaluate/renderização condicional que pode ser utilizada dentro do JSX - Apenas */}
+    {/* Se loading for  TRUE, renderize  <Loading />*/}
+      { loading && <Loading />}
+
+      <ScrollView>
+        <View style={estilos.viewFilmes}>
+            {/* Se loading for false, renderize o resultado de map */}
+        {!loading &&
+      resultados.map(({id, poster_path, title}) => (
+
+            <View key={id}>
+                <Text> {title} </Text>
+                <Image style={estilos.image} source={{uri: `https://image.tmdb.org/t/p/original/${poster_path}`}}/>
+            </View>
+
       ))}
-      <Image source={resultados.poster_path}/>
+       </View>
+       </ScrollView>
     </SafeAreaView>
   )
 }
@@ -67,6 +78,14 @@ const estilos = StyleSheet.create({
     container: {
         flex: 1,
         padding: 16,
+    },
+    viewFilmes: {
+
+    },
+    image: {
+        height: 100,
+        width: 100,
+        textAlign: "center",
     },
    
 });
