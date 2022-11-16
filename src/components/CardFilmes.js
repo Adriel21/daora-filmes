4223e68cbd7ffb9aca02ccc2636f17566f2cb0b6
Adriel Lira <adrieldiasdossantoslira@hotmail.com>
2022-11-16 09:39:03 -0300

    Construção e estilização do card

--- src/components/CardFilmes.js
+++ src/components/CardFilmes.js
@@ -1,13 +1,26 @@
-import { StyleSheet, Text, View, Image } from 'react-native'
+import { StyleSheet, Text, View, Image, Pressable } from 'react-native'
+import { Ionicons } from '@expo/vector-icons';
 import React from 'react'
 
 const CardFilmes = ({filme}) => {
     const {title, poster_path} = filme;
 
   return (
-    <View>
-        <Text> {title} </Text>
-        <Image style={estilos.image} source={{uri: `https://image.tmdb.org/t/p/original/${poster_path}`}}/>
+    
+      <View style={estilos.card}>
+          <Image style={estilos.image}  resizeMode="cover" source={{uri: `https://image.tmdb.org/t/p/original/${poster_path}`}}/>
+        <View style={estilos.corpo}>
+          <Text style={estilos.titulo}> {title} </Text>
+          <View style={estilos.botoes}>
+            <Pressable style={estilos.botao}>
+              <Text style={estilos.textoBotao}> <Ionicons name="book" size={12} color="#5451a6" /> Leia mais</Text>
+            </Pressable>
+            <Pressable style={estilos.botao}>
+              <Text style={estilos.textoBotao}><Ionicons name="add-circle" size={12} color="#5451a6" /> Salvar</Text>
+            </Pressable>
+        </View>
+          <View></View>
+        </View>
     </View>
   )
 }
@@ -15,10 +28,49 @@ const CardFilmes = ({filme}) => {
 export default CardFilmes
 
 const estilos = StyleSheet.create({
+    card: {
+      marginVertical: 4,
+      flexDirection: "row",
+      borderWidth: 1,
+      borderStyle: "solid",
+      borderColor: "black",
+      justifyContent: "center",
+      // alignItems: "center"
+    },
     image: {
-        height: 500,
-        width: 300,
+        height: 150,
+        width: 100,
+       
 
     },
+    corpo: {
+      flex: 2,
+    },
+    titulo:{
+      backgroundColor: "#5451a6",
+      color: "white",
+      fontSize: 16,
+      paddingVertical: 8,
+      textAlign: "center"
+    },
+    botoes: {
+      flexDirection: "row",
+      justifyContent: "space-evenly",
+      marginTop: 8,
+    },
+    botao: {
+      padding: 12,
+      borderWidth: 1,
+      borderColor: "#5451a6"
+    },
+    textoBotao: {
+      color: "#5451a6",
+      fontSize: 12,
+      textTransform: "uppercase",
+      textAlign: "center",
+      fontWeight: "semibold",
+      justifyContent: "center"
+    }
+   
    
 })
